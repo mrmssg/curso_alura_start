7381e39d376399a9612bd8051602d1bde74704c5
Mauro S S Goncalves <133293974+mrmssg@users.noreply.github.com>
2024-04-26 18:56:35 -0300

    01

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-let nomeUsuario = prompt("Qual é o seu nome?");
+let nomeUsuario = "";
 let elemento = document.querySelector("#nome-do-usuario");
+
+while (nomeUsuario =="") {
+    nomeUsuario = prompt("Qual é o seu nome?")
+}
+
 elemento.textContent = nomeUsuario;
